24a5fc82ab1d851cc180b7b2a76326606eb09b2a
alkamaazmi <alkamaazmi07@gamil.com>
2022-07-08 12:02:56 +0530

    Rotate the array right by k steps

---
+++ Array/189.rotate-array.cpp
@@ -0,0 +1,31 @@
+/* leetcode 189.rotare-array
+  rotate the array right by k steps
+*/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        k=k%nums.size();
+
+        //reverse first n-k elements
+        reverse(nums, 0,nums.size()-k-1);
+
+        //reverse last k elements
+        reverse(nums, nums.size()-k ,nums.size()-1);
+
+        //reverse the whole array 
+        reverse(nums,0,nums.size()-1);
+    }
+    //function to reverse an array
+    void reverse(vector<int>& nums, int low, int high)
+    {
+        while(low<high)
+        {
+            swap(nums[low],nums[high]);
+            low++;
+            high--;
+        }
+    }
+};
+//Time complexity is O(n)
+//space comlexity is O(1)
