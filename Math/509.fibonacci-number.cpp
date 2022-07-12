236be26026bca0c35ce1354c6c07a4f1dee05344
alkamaazmi <alkamaazmi07@gamil.com>
2022-07-12 13:28:26 +0530

    fibonacci-number

---
+++ Math/509.fibonacci-number.cpp
@@ -0,0 +1,20 @@
+/*
+ * [509] Fibonacci Number
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones
+ * Fn = Fn-1 + Fn-2
+ */
+
+class Solution {
+public:
+    int fib(int n) {
+        float z = sqrt(5);
+        long long int t = (pow(2,N)*z);
+        long long int a = (pow(1+z,N));
+        long long int b = (pow(1-z,N));
+        return (a-b)/t;
+    }
+};
+// Fn = {[(√5 + 1)/2] ^ n} / √5 
+// Time Complexity: O(logn), this is because calculating phi^n takes logn time
+// Space Complexity: O(1)
+
