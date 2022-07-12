9d881b1479e50c2b6f004d6b661941fdbf797beb
alkamaazmi <alkamaazmi07@gamil.com>
2022-07-12 12:40:47 +0530

    258.add-digits.cpp

---
+++ Math/258.add-digits.cpp
@@ -0,0 +1,24 @@
+/*
+ * leetcode [258] Add Digits
+ * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+ */
+
+class Solution {
+public:
+    int addDigits(int num) {
+
+         if(num==0){
+            return num;
+         }
+
+         if(num%9==0){
+            return 9;
+         }
+         
+         return num%9;
+    }
+};
+
+Time Complexity : O(1)
+
+
