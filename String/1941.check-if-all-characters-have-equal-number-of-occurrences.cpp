6541bf48cb0af9b7a4596a000f6f36a64c0ec9a1
alkamaazmi <alkamaazmi07@gamil.com>
2022-07-12 11:51:44 +0530

    check-if-all-characters-have-equal-number-of-occurrences

---
+++ String/1941.check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,24 @@
+/*
+ * [1941] Check if All Characters Have Equal Number of Occurrences
+ * Given a string s, return true if s is a good string, or false otherwise
+ * A string s is good if all the characters that appear in s have the same number of occurrences
+ */
+
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+         int a[26]={0};
+        for(auto i:s){
+            a[i-'a']++;
+        }
+        int occur=a[s[0]-'a'];
+        for(int i=1;i<s.size();i++){
+            if(a[s[i]-'a']!=occur){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
