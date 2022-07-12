2f0e1d8099e11c4493f4573ff7dcbd4fd6753095
alkamaazmi <alkamaazmi07@gamil.com>
2022-07-12 23:17:20 +0530

    two-sum

--- Array/1.two-sum.cpp
+++ Array/1.two-sum.cpp
@@ -1,15 +1,30 @@
 /*
- * @lc app=leetcode id=1 lang=cpp
- *
- * [1] Two Sum
+ * leetcode [1] Two Sum
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
  */
 
-// @lc code=start
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n=nums.size();
+        vector<int> v;
+        unordered_map<int,int> m;
         
+        for(int i=0;i<n;i++){
+           int a=target-nums[i];
+            if(m.find(a)!=m.end()){
+                v.push_back(i);
+                v.push_back(m[a]);
+            }else{
+                m[nums[i]]=i;
+            }
+        }
+        return v;
     }
 };
-// @lc code=end
+
+//Time Complexity:O(n)
+//Space Complexity:O(n)
+
 
