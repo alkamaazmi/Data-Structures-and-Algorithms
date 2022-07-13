fb3f889582609883fa62ccc0f1d7dd0069d42e0e
alkamaazmi <74311592+alkamaazmi@users.noreply.github.com>
2022-07-13 11:20:26 +0530

    Update 1679.max-number-of-k-sum-pairs.cpp

--- Array/1679.max-number-of-k-sum-pairs.cpp
+++ Array/1679.max-number-of-k-sum-pairs.cpp
@@ -1,5 +1,5 @@
 /*
- * leetcode [1679] Max Number of K-Sum Pairs
+ * Leetcode [1679] Max Number of K-Sum Pairs
  * In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
  * Return the maximum number of operations you can perform on the array.
  */
