4be40e4c68c11f552f8e6271781e17f9cd3cecb6
alkamaazmi <alkamaazmi07@gamil.com>
2022-07-14 12:51:24 +0530

    Time O(logN)

---
+++ Math/1281.subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1281 lang=cpp
+ *
+ * [1281] Subtract the Product and Sum of Digits of an Integer
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        
+    }
+};
+// @lc code=end
+
