70fc41313e78b3423e92854c1acf3aaf4d13235f
alkamaazmi <alkamaazmi07@gamil.com>
2022-08-21 14:48:22 +0530

    majority-element-ii

---
+++ Array/229.majority-element-ii.cpp
@@ -0,0 +1,52 @@
+/*
+ * Leetcode [229] Majority Element II
+ * Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times
+ */
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int sz = nums.size();
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+        for (i = 0; i < sz; i++)
+        {
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+            else if (count1 == 0)
+            {
+                num1 = nums[i];
+                count1 = 1;
+            }    
+            else if (count2 == 0)
+            {
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> ans;
+        count1 = count2 = 0;
+        for (i = 0; i < sz; i++)
+        {
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+        }
+        if (count1 > sz/3)
+            ans.push_back(num1);
+        if (count2 > sz/3)
+            ans.push_back(num2);
+        return ans;
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
