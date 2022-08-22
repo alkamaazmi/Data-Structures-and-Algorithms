b10cec10e04bf91a9a1d909d5306cf853ce72b1f
alkamaazmi <alkamaazmi07@gamil.com>
2022-08-22 13:43:38 +0530

    best-time-to-buy-and-sell-stock

--- Array/121.best-time-to-buy-and-sell-stock.cpp
+++ Array/121.best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,23 @@
 /*
  * Leetcode [121] Best Time to Buy and Sell Stock
- * Given an integer numRows, return the first numRows of Pascal's triangle
- * In Pascal's triangle, each number is the sum of the two numbers directly above it
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0
  */
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> ret;
-	    for (int i = 0; i < numRows; i++) {
-		    vector<int> row(i + 1, 1);
-		    for (int j = 1; j < i; j++) {
-			    row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
-		    }
-		    ret.push_back(row);
-	    }
-	    return ret;
+        int minprice=INT_MAX;
+        int maxprofit=0;
+        for(int i=0;i<prices.size();i++){
+            minprice=min(minprice,prices[i]);
+            maxprofit=max(maxprofit,prices[i]-minprice);
+        }
+        return maxprofit;
     }
-}
+};
 
-// Time Complexity : O(n^2)
-// Space Complexity : O(n^2)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
