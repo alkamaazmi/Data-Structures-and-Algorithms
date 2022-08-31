9f1b09c244defde6a483f10fd49e350a1c678684
alkamaazmi <alkamaazmi07@gamil.com>
2022-08-31 19:22:15 +0530

    Print-Matrix-in-snake-Pattern

---
+++ Matrix/Print-Matrix-in-snake-Pattern.cpp
@@ -0,0 +1,30 @@
+/*
+ * GeeksForGeeks : Print Matrix in snake Pattern
+ * Given a matrix of size N x N
+ * Print the elements of the matrix in the snake like pattern
+ */
+a
+class Solution
+{   
+    public:
+    //Function to return list of integers visited in snake pattern in matrix.
+    vector<int> snakePattern(vector<vector<int> > matrix)
+    {   
+        vector<int> ans;
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                for(int j=0;j<n;j++){
+                    ans.push_back(matrix[i][j]);
+                }
+            }else{
+                for(int j=n-1;j>=0;j--){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Time Complexity: O(N*N)
