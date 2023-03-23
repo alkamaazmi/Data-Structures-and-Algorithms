64346805b42d4c26dc241076ef455ef633ad55b3
Alkama Azmi <alkamaazmi07@gmail.com>
2023-03-23 19:36:03 +0530

    Reverse a Stack

---
+++ Stack/Reverse a Stack.cpp
@@ -0,0 +1,34 @@
+/*
+ * You are given a stack St. You have to reverse the stack using recursion.
+ */
+
+class Solution{
+public:
+    
+    
+    void insertAtBottom(stack<int> &St,int x){
+        if(St.empty()){
+            St.push(x);
+            return;
+        }
+        int num=St.top();
+        St.pop();
+        insertAtBottom(St,x);
+        St.push(num);
+    }
+
+
+    void Reverse(stack<int> &St){
+        if(St.empty()){
+            return;
+        }
+        int num=St.top();
+        St.pop();
+        Reverse(St);
+        
+        insertAtBottom(St,num);
+    }
+};
+
+//Time Complexity:O(n)
+//Space Complexity:O(n)
