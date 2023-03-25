73272cabf40d024d940c10693eac0fba404b75bb
Alkama Azmi <alkamaazmi07@gmail.com>
2023-03-25 19:23:37 +0530

    Insert into a Binary Search Tree

---
+++ Binary Search Tree/701.insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,20 @@
+/*
+ * [701] Insert into a Binary Search Tree
+ */
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return new TreeNode(val);
+        }
+        if(val<root->val){
+            root->left=insertIntoBST(root->left,val);
+        }else{
+            root->right=insertIntoBST(root->right,val);
+        }
+        return root;
+    }
+};
+
+
